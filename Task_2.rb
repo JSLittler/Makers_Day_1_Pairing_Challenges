eed9848a5ad143e2843489974d2d85cb5e261800
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-01 16:22:14 +0100

    First commit Task 2 - A - rspec

---
+++ Task_2.rb
@@ -0,0 +1,6 @@
+def add_hash(argument)
+hash = { a: 1, b: 2 }
+puts hash
+end
+
+add_hash(hash)
---
+++ spec/T2_spec.rb
@@ -0,0 +1,7 @@
+require_relative '../Task_2'
+
+describe 'add_hash' do
+  it 'returns "number" when passed letter' do
+    expect(add_hash(a)).to eq(2)
+  end
+end
