bc14c1b170245425415e8dcaa37772f8cb5766de
John Littler <johnlittler@bgl-mac-008785.bgl.financial.local>
2018-10-02 09:24:07 +0100

    Task 2 updated for second part of task - hash sort. currently passes both tests.

--- Task_2.rb
+++ Task_2.rb
@@ -1,7 +1,11 @@
 def add_hash(arg)
-hash = { "a" => 1, "b" => 2 }
-hash.each {|k, v| hash[k] = v + 1}
-return hash[arg]
+  hash = { "a" => 1, "b" => 2 }
+  hash.each {|k, v| hash[k] = v + 1}
+  return hash[arg]
 end
 
-add_hash("a")
+def sort_hash
+  hash_2 = { "a" => 2, "b" => 5, "c" => 1 }
+  hash_sort = hash_2.sort
+  return hash_sort.to_h
+end
--- spec/T2_spec.rb
+++ spec/T2_spec.rb
@@ -1,7 +1,13 @@
 require_relative '../Task_2'
 
 describe 'add_hash' do
-  it 'returns "2" when passed "a"' do
+  it 'returns "number" when passed "a"' do
     expect(add_hash("a")).to eq 2
+  end
+end
+
+describe 'sort_hash' do
+  it 'returns "sorted hash"' do
+    expect(sort_hash).to eq "c" => 1, "a" => 2, "b" => 5
   end
 end
